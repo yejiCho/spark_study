c05bfbf727bf813aad90ee4fe9232468cf5c4f72
yejiCho <goe152@naver.com>
2020-08-17 20:42:37 +0900

    spark+kafka

--- python/ch06/kafka_sample.py
+++ python/ch06/kafka_sample.py
@@ -1,19 +1,23 @@
-import findspark
+# import findspark
 from pyspark import SparkContext, SparkConf, storagelevel
 from pyspark.streaming.context import StreamingContext
 
 # NOTE pyspark version
 # pyspark 3.0 부터는 kafka 없음
 # pyspark 2.0대 사용
+# from pyspark.streaming.kafka import KafkaUtils
 from pyspark.streaming.kafka import KafkaUtils
+import os
 
-findspark.init()
+# findspark.init()
+os.environ['PYSPARK_PYTHON'] = '/usr/bin/python3'
+os.environ['PYSPARK_DRIVER_PYTHON'] = '/usr/bin/python3'
+
 conf = SparkConf()
-sc = SparkContext(master="local[*]",appName="KafkaSample",conf=conf)
+sc = SparkContext(master="local",appName="KafkaSample",conf=conf)
 ssc = StreamingContext(sc,3)
 
 ds1 = KafkaUtils.createStream(ssc,"localhost:2181","testGroup",{"test":3})
-
 ds2 = KafkaUtils.createDirectStream(ssc,["test"],{"metadata.broker.list":"localhost:9092"})
 
 ds1.pprint()
